4773d881e6244073599282ffc9bcf06f57fce28a
ciroalo <ciroalo2000@gmail.com>
2023-08-23 00:15:56 +0200

    added gui functionality

--- sudoku.py
+++ sudoku.py
@@ -1,36 +1,47 @@
 import numpy as np
+import pygame
+
+WIDTH = 550     # Width-height of the gui
+
+# Background color
+BACKGROUND_COLOR = (251, 247, 245)
+
 
 class Sudoku:
-    def __init__(self, initial_board, help=True) -> None:
+    def __init__(self, initial_board, help=True, gui=False):
         """
         Initialize a Sudoku puzzle.
 
         Args:
             initial_board (numpy.ndarray): A 9x9 NumPy array representing the initial Sudoku board.
             help (bool, optional): Enable or disable help mode. Defaults to True.
+            gui (bool, optional): Enable or disable GUI. Defaults to False. 
         
         Raises:
             ValueError: If the initial board is invalid.
         """
         self.help = help
+        self.gui = gui  # Now 'gui' is a keyword argument that can be set during instantiation.
         
         if initial_board is None:
             raise ValueError("Invalid initial board. Initial board cannot be None.")
         else:
             if initial_board.shape == (9, 9) and np.all((0 <= initial_board) & (initial_board <= 9)):
-                self.board = initial_board
+                self.initial_board = initial_board
+                self.board = np.copy(initial_board)
                 self.copy_board = np.copy(initial_board)
                 
                 if self.solve():
-                    # there was a solution for the iniital_board
-                    self.initial_board = initial_board
+                    # there was a solution for the initial_board
                     self.resolved_board = self.board
                     self.board = self.copy_board
+                    if self.gui:
+                        self.__start_gui()
                 else:
                     raise ValueError("Invalid initial board. It doesn't have a solution or has multiple solutions.")
             else:
-                raise ValueError("Invalid initial board. It must be a 9x9 NumPy array with values between 0 and 9.")          
-        
+                raise ValueError("Invalid initial board. It must be a 9x9 NumPy array with values between 0 and 9.")
+
     
     def display(self):
         """
@@ -217,3 +228,73 @@ class Sudoku:
             mode (bool): True to enable help mode, False to disable it.
         """
         self.help = mode
+        
+        
+    def __insert_gui(self, win, position):
+        i, j = position[1], position[0]
+        myfont = pygame.font.SysFont('Comic Sans Ms', 35)
+        buffer = 5
+        
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
+                if event.type == pygame.KEYDOWN:
+                    if(self.initial_board[i-1][j-1] != 0):
+                        return
+                    if(event.key == 48):
+                        if self.delete_value(i-1, j-1) > 0:
+                            pygame.draw.rect(win, BACKGROUND_COLOR, (j*50 + buffer, i*50+ buffer, 50 - 2*buffer, 50 - 2*buffer))
+                            pygame.display.update()
+                        return
+                        
+                    elif(0 < event.key - 48 <10):  #We are checking for valid input
+                        if self.insert(i-1, j-1, int(event.key - 48)):
+                            pygame.draw.rect(win, BACKGROUND_COLOR, (j*50 + buffer, i*50+ buffer, 50 - 2*buffer, 50 - 2*buffer))
+                            value = myfont.render(str(event.key-48), True, (0,0,0))
+                            win.blit(value, ((j)*50 + 15, (i)*50 + 15))
+                            pygame.display.update()
+                            
+                        return
+                    return
+        
+        
+    def __start_gui(self):
+        pygame.init()
+        win = pygame.display.set_mode((WIDTH, WIDTH))
+        pygame.display.set_caption("Sudoku")
+        win.fill(BACKGROUND_COLOR)
+        initial_font = pygame.font.SysFont('Comic Sans Ms', 35)
+        color_initial_font = (52, 31, 151)
+        
+        # Draw grid
+        for i in range(0, 10):
+            if i%3 == 0:
+                # thicker lines for box separator
+                pygame.draw.line(win, (0,0,0), (50 + 50*i, 50), (50 + 50*i, 500), 4)
+                pygame.draw.line(win, (0,0,0), (50, 50 + 50*i), (500, 50 + 50*i), 4)
+                
+            pygame.draw.line(win, (0,0,0), (50 + 50*i, 50), (50 + 50*i, 500), 2)
+            pygame.draw.line(win, (0,0,0), (50, 50 + 50*i), (500, 50 + 50*i), 2)
+        pygame.display.update()
+        
+        # Populate grid with board
+        for i in range(9):
+            for j in range(9):
+                if (0 < self.board[i][j] < 10):
+                    value = initial_font.render(str(self.board[i][j]), True, color_initial_font)
+                    win.blit(value, ((j+1)*50 + 15, (i+1)*50 + 15))
+        pygame.display.update()
+        
+        while True: 
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                    pos = pygame.mouse.get_pos()
+                    self.__insert_gui(win, (pos[0]//50, pos[1]//50))
+                    if self.__find_empty_cell() is None:
+                        print("You have succesfully finished the game!")
+                        pygame.quit()
+                        return
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    return
